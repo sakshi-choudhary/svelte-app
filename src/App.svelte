d3ab01bbb5a886bd6f4ecd8474eab968e8623035
Sakshi Choudhary <choudharysakshi023@gmail.com>
2021-06-09 20:04:23 +0530

    ✨ counter app

--- src/App.svelte
+++ src/App.svelte
@@ -3,8 +3,17 @@
   function fetchMe() {
     console.log("Hello world!");
   }
+
+  let count = 0;
+  const counter = () => {
+    count++;
+  };
+
 </script>
+
 <main>
   <h1>{name}</h1>
   <button on:click={fetchMe}>Fetch Data!</button>
+  <button on:click={counter}>counter</button>
+  <h2>{count}</h2>
 </main>
